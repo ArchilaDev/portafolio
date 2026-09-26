778bf1271be6bbb1a60abcaf2e301a8b55a56095
ArchilaDev <mrarchilag@gmail.com>
2026-09-26 09:55:55 -0500

    Agregacion JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 const botonTema = document.getElementById('cambio_tema');
 const iconoTema = botonTema.querySelector('i');
+const sonidoClick = new Audio("sounds/clickSound.mp3");
 
 botonTema.addEventListener('click', (e) => {
-    e.preventDefault(); // 'D' mayúscula corregida
+    e.preventDefault(); 
 
-    // Alterna la clase en el body (la pone si no está, la quita si ya está)
     document.body.classList.toggle('light-mode');
-
+    sonidoClick.currentTime = 0;
+    sonidoClick.play();
     // Comprueba el estado actual para cambiar el icono
     if (document.body.classList.contains('light-mode')) {
         iconoTema.classList.remove('fa-moon-o');
         iconoTema.classList.add('fa-sun-o');
-        iconoTema.style.color = '#F59E0B'; // Sol dorado
+        iconoTema.style.color = '#F59E0B'; 
     } else {
         iconoTema.classList.remove('fa-sun-o');
         iconoTema.classList.add('fa-moon-o');
-        iconoTema.style.color = 'white'; // Luna blanca
+        iconoTema.style.color = 'white'; 
     }
 });
+
+
